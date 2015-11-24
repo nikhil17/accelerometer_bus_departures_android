15c82862798e1eaa5e6744ca7cc6d6f3a9dd4893
nkarajgikar6 <nikhilkarajgikar@gmail.com>
2015-11-24 11:42:48 -0500

    Readded libs folder

--- app/src/main/java/com/example/livemap/accelerometertest/MainActivity.java
+++ app/src/main/java/com/example/livemap/accelerometertest/MainActivity.java
@@ -13,7 +13,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
 import java.text.SimpleDateFormat;
